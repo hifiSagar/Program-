c526670ac9b162b4cfeff41bcd557557c2a50ac9
SAGAR T <ssagu6661@gmail.com>
2024-09-27 19:04:51 +0530

    Add files via upload

---
+++ basic_program14.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+    int a,b;
+                            // it have a sum bug, What is the problem i don't know.
+const [a, ,b] = [1,2,3];    
+console.log(a,b);
+
+}
